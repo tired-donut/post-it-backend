860ba3aa2522570fca0702f076e0b3ca6a182bb0
Diego <diego.reeis26@gmail.com>
2024-12-18 20:39:31 -0300

    fix(posts): remover excecao nao utilizada

--- src/modules/posts/posts.service.ts
+++ src/modules/posts/posts.service.ts
@@ -1,4 +1,4 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Post } from './entities/post.entity';
 import { Repository } from 'typeorm';
